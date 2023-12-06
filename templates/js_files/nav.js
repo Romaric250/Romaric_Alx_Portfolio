afc70ecef1ccffd2b434763c04b27e34e1cea69c
Romaric250 <lonfonyuyromeo@gmail>
2023-12-05 13:59:45 -1100

    some task done

---
+++ templates/js_files/nav.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var dropmenu = document.getElementById("drop-down"),
+    mediaButton = document.getElementById("mediaButton");
+
+mediaButton.onclick = function () {
+    
+    
+    
+    dropmenu.classList.toggle("show_list");
+    mediaButton.classList.toggle("active");
+    
+};
+
+//slider container
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); 
+}
